4ad0a3fa90ddea5cef35fad152edf059846759f2
Shivakumara K S <shivakumara.ks08@gmail.com>
2026-08-28 17:06:26 +0530

    Initial commit

---
+++ src/test/java/com/example/lru_cache_service/LruCacheServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lru_cache_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LruCacheServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
